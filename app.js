189d99715d1d821fcd7792921d754203bce1fdf2
SandhanuDulmeth <sandhanu12@gmail.com>
2024-11-06 16:58:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+
+
+loadItems();
+    
+    async function loadItems() {
+       
+let searchtxt=document.getElementById("search").value;
+
+        let res = await fetch(`https://restcountries.com/v3.1/name/${searchtxt}`);
+        let items = await res.json();
+        let body = "";
+        items.forEach(element => {
+            console.log(element);
+            body+=`
+                <div class="col">
+                    <div class="card shadow-sm">
+                      <img src=${element.flags.png} alt="">
+                      <div class="card-body">
+                      <h2>${element.name.common}</h2>
+                        <p class="card-text">Capital : ${element.capital}</p>
+                        <p class="card-text">Region : ${element.region	}</p>
+                        <p class="card-text">Population : ${element.population}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                          <div class="btn-group">
+                           <a href=${element.maps.googleMaps} class="btn btn-primary" target="_blank">View on Google Maps</a>
+                            
+                          </div>
+                         
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            `;
+    
+            
+            
+        });
+
+    console.log(body);
+
+    document.getElementById("row").innerHTML=body;
+    
+};
+
+
+
+
+
